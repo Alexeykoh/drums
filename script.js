224cd239004186ebb8a4c6976c27b9d7bb7b377c
Alexeykoh <Alexeykoh@icloud.com>
2022-05-10 21:34:46 +0300

    feat: drums

---
+++ script.js
@@ -0,0 +1,44 @@
+let audioKick = new Audio('assets/audio/Bass-Drum-1.wav');
+let audioSnare = new Audio('assets/audio/snare.wav');
+let audioHH = new Audio('assets/audio/opHat.wav');
+let audioHH_close = new Audio('assets/audio/clHat.wav');
+let crash = new Audio('assets/audio/allCrash.wav');
+let maxVol = 0.5;
+
+document.querySelector('#kick').addEventListener("click", clickKick)
+document.querySelector('#snare').addEventListener("click", clickSnare)
+document.querySelector('#hh').addEventListener("click", clickHH)
+document.querySelector('#hh-close').addEventListener("click", clickHH_close)
+document.querySelector('#crash').addEventListener("click", clickCrash)
+
+function clickKick (){
+    audioKick.pause();
+    audioKick.volume = maxVol;
+    audioKick.currentTime = 0;
+    audioKick.play();
+}
+function clickSnare (){
+    audioSnare.pause();
+    audioSnare.volume = maxVol;
+    audioSnare.currentTime = 0;
+    audioSnare.play();
+}
+function clickHH (){
+    audioHH.pause();
+    audioHH.volume = maxVol;
+    audioHH.currentTime = 0;
+    audioHH.play();
+}
+function clickHH_close (){
+    audioHH_close.pause();
+    audioHH_close.volume = maxVol;
+    audioHH_close.currentTime = 0;
+    audioHH_close.play();
+}
+function clickCrash (){
+    crash.pause();
+    crash.volume = maxVol;
+    crash.currentTime = 0;
+    crash.play();
+}
+
